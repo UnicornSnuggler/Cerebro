a5a532a57a8a76848c8d931cc12c041c0063980f
UnicornSnuggler <logan@willisweb.com>
2025-08-17 13:48:26 -0500

    Bulk modification sample and per group icon support.

---
+++ bulk-modifications.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const { CardEntity } = require('./models/cardEntity');
+const { RuleEntity } = require('./models/ruleEntity');
+const { CreateDocumentStore, DeriveDatabase } = require('./utilities/documentStoreHelper');
+const { ALL } = require('./constants');
+
+const store = CreateDocumentStore(DeriveDatabase(RuleEntity.DATABASE_SUFFIX)).initialize();
+
+// async function execute() {
+//     let session = store.openSession();
+//     let cardEntities = await session.query({ indexName: `${ALL}${CardEntity.COLLECTION}` }).all();
+    
+//     for (card of cardEntities) {
+//         if (!card.Queries || card.Queries == null) {
+//             card.Queries = 0;
+//         }
+//     }
+    
+//     await session.saveChanges();
+// }
+
+async function execute() {
+    let session = store.openSession();
+    let ruleEntities = await session.query({ indexName: `${ALL}${RuleEntity.COLLECTION}` }).all();
+    
+    let count = 0;
+
+    for (rule of ruleEntities) {
+        if (!rule.Queries || rule.Queries == null) {
+            rule.Queries = 0;
+            count++;
+        }
+    }
+
+    await session.saveChanges();
+    
+    console.log(`Done! ${count} entries modified...`);
+}
+
+console.log(`Executing...`);
+
+execute();
